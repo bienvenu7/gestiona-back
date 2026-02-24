f2fe2e6c19d5d29aed0e0fce4c6bd9a4f3efdcfc
bienvenu7 <ngadzieb@gmail.com>
2026-02-25 00:29:07 +0300

    to gestiona

--- source/config/mailer.config.ts
+++ source/config/mailer.config.ts
@@ -5,24 +5,32 @@ import { logger } from '../utils/logger';
 
 export async function sendHash(hash: string, email: string) {
   try {
+    // const transporter = nodemailer.createTransport({
+    //   host: 'mail.hosting.reg.ru',
+    //   port: 465, // Port sécurisé (SSL)
+    //   secure: true, // true pour 465, false pour 587 (TLS)
+    //   auth: {
+    //     user: 'noreply@afrue.com',
+    //     pass: 'cN1eS3bQ1gyK7sD3',
+    //   },
+    //   tls: {
+    //     // 🔥 CRITIQUE POUR REG.RU
+    //     servername: 'mail.hosting.reg.ru',
+    //     rejectUnauthorized: false,
+    //     minVersion: 'TLSv1',
+    //   },
+    //   connectionTimeout: 20_000,
+    //   greetingTimeout: 20_000,
+    //   socketTimeout: 20_000,
+    //   logger: true,
+    // });
+
     const transporter = nodemailer.createTransport({
-      host: 'mail.hosting.reg.ru',
-      port: 465, // Port sécurisé (SSL)
-      secure: true, // true pour 465, false pour 587 (TLS)
+      service: 'mail.ru',
       auth: {
-        user: 'noreply@afrue.com',
-        pass: 'cN1eS3bQ1gyK7sD3',
+        user: 'durel7@mail.ru',
+        pass: process.env.MAIL_RU_APP,
       },
-      tls: {
-        // 🔥 CRITIQUE POUR REG.RU
-        servername: 'mail.hosting.reg.ru',
-        rejectUnauthorized: false,
-        minVersion: 'TLSv1',
-      },
-      connectionTimeout: 20_000,
-      greetingTimeout: 20_000,
-      socketTimeout: 20_000,
-      logger: true,
     });
 
     // Send email
