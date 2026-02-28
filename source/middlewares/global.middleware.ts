318f67963934e09c105c3439c415661cf16766d4
bienvenu7 <ngadzieb@gmail.com>
2026-02-28 14:31:14 +0300

    f

--- source/middlewares/global.middleware.ts
+++ source/middlewares/global.middleware.ts
@@ -71,8 +71,11 @@ export const compressionConfig: RequestHandler = compression({
 
 // CORS configuration
 export const corsConfig = cors({
-  // origin: 'https://www.inventera.pro',
-  origin: 'http://localhost:3000',
+  origin:
+    process.env.NODE_ENV !== 'production'
+      ? 'http://localhost:3000'
+      : 'https://www.inventera.pro',
+  // origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
